4eafc3f6211e80761729916a40097aa8f775c19a
Pavel <ya.skillet-02@ya.ru>
2024-01-24 11:36:02 +0300

    В админ зону добавлены модели User, Category, Title, Review.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Category, Title, Review
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'description',
+    )
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'user_id',
+        'text',
+        'rating',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Review, ReviewAdmin)
