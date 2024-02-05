886a105e0c639a903e646edd1e1fdcc9ed8762a4
Pavel <ya.skillet-02@ya.ru>
2024-02-05 12:07:00 +0300

    Исправлены отзывы и комменты после ревью

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -202,17 +202,16 @@ class ReviewSerializer(serializers.ModelSerializer):
         slug_field='username'
     )
 
-    def validate(self, attrs):
-        request = self.context.get('request')
+    def validate(self, data):
+        request = self.context['request']
         author = request.user
-        title_id = self.context.get('view').kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
+        title_id = self.context['view'].kwargs.get('title_id')
         if request.method == 'POST':
-            if Review.objects.filter(author=author, title=title).exists():
+            if Review.objects.filter(author=author, title=title_id).exists():
                 raise serializers.ValidationError(
                     'Вы не можете создать больше одного отзыва на произведение'
                 )
-        return attrs
+        return super().validate(data)
 
     class Meta:
         model = Review
