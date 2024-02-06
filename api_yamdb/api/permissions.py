71ae89ed86adbe43257557d1b095bb1f6e9100f5
Pavel <ya.skillet-02@ya.ru>
2024-02-06 10:27:48 +0300

    Удалено лишнее право модератора

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -23,17 +23,6 @@ class IsAdminOrReadOnly(BasePermission):
         )
 
 
-class ModerAuthenticatedOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS or
-                request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS or
-                (request.user.is_authenticated and
-                 (obj.author == request.user or request.user.is_moderator)))
-
-
 class ReviewCommentPermissions(BasePermission):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
